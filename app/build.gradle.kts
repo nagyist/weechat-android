d036a4b489920929890ec7e56d1442e4e8c9758c
oakkitten <oakkitten@users.noreply.github.com>
2025-10-31 15:17:47 +0000

    Enable resource shrinking
    Also use the new option `android.r8.optimizedResourceShrinking=true`.
    
    This reduces APK size by 8.9%, from 3,078,458 bytes to 2,804,784 bytes.
    
    The optimized resource shrinking option should be especially useful if we
    use any third-party widget libraries such as the material library which
    includes snackbars. AAPT generates a lot of unconditional rules for the
    widgets, and it's extremely hard to get rid of them without this option.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,6 +105,7 @@ android {
 
         getByName("release") {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
                           "proguard-rules.pro",
                           "../cats/proguard-rules.pro")
